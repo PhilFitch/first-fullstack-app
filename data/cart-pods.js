b4a324ba203e6b63b9c036be02d326a028ad670b
Phil Fitch <mostreluctantly@gmail.com>
2019-08-23 16:30:06 -0700

    file setup and data mockup

---
+++ data/cart-pods.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        name: 'Killingsworth Station Food Carts',
+        no-of-carts: 9,
+        seating: true,
+        location: 'North'
+    },
+    {
+        name: 'Piedmont Station Food Cart Pod',
+        no-of-carts: 13,
+        seating: true,
+        location: 'Northeast'
+    },
+    {
+        name: 'Cartopia',
+        no-of-carts: 9,
+        seating: true,
+        location: 'Southeast'
+    },
+    {
+        name: '3rd Avenue Food Cart Pod',
+        no-of-carts: 12,
+        seating: false,
+        location: 'Southwest'
+    },
+    {
+        name: '5th Avenue Food Cart Pod',
+        no-of-carts: 33,
+        seating: false,
+        location: 'Southwest'
+    },
+    {
+        name: 'Rose City Food Park',
+        no-of-carts: 15,
+        seating: true,
+        location: 'Northeast'
+    },
+];
